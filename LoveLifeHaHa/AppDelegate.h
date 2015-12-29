ff2ec0f22c768e9bfc81dbbe60feaad7f081f1b1
qianfeng <7890xyz123@sina.cn>
2015-12-29 09:31:40 +0800

    Initial Commit

---
+++ LoveLifeHaHa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoveLifeHaHa
+//
+//  Created by qianfeng on 15/12/29.
+//  Copyright © 2015年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
